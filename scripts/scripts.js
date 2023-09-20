a6f6207423cfe9c2b4b6d2e1b4124dfccdb9f48b
Tucker Elliott <120062499+tuckerelliott@users.noreply.github.com>
2023-09-20 15:12:43 -0400

    remove autoblock

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -45,7 +45,7 @@ async function loadFonts() {
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
- */
+ 
 function buildAutoBlocks(main) {
   try {
     buildHeroBlock(main);
@@ -54,7 +54,7 @@ function buildAutoBlocks(main) {
     console.error('Auto Blocking failed', error);
   }
 }
-
+*/
 /**
  * Decorates the main element.
  * @param {Element} main The main element
